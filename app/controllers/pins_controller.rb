69ad50a28c09af0ac885afd1c38a6085a8d3ccaf
Germain Boue <boue.germain@gmail.com>
2015-01-06 18:21:15 -0800

    Added a form to create new pin in view as well as added a show method and private method in order to find pin when we will run actions show/update/destroy

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -6,11 +6,21 @@ class PinsController < ApplicationController
 
   def new 
     @pin = Pin.new
-  
+  end
+
+  def show
+
   end
 
   def create
     @pin = Pin.new(pin_params)
+
+    if @pin.save 
+      redirect_to @pin, notice: "Successfully created pin."
+    else
+      render 'new'
+    end
+    
   end
 
   private
@@ -19,4 +29,8 @@ class PinsController < ApplicationController
     params.require(:pin).permit(:title, :description)
   end
 
+  def find_pin
+    @pin = Pin.find(params[:id])
+  end
+  
 end
